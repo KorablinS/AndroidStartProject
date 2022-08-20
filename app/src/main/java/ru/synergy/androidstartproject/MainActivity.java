d5704d9b3f4aec95dc864ab90750df6c34f216d2
KorablinS <korablin198421@gmail.com>
2022-08-20 16:26:02 +0300

    Added Java screen configuration example with Constraint Layout

--- app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
+++ app/src/main/java/ru/synergy/androidstartproject/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.synergy.androidstartproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -13,13 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
         TextView textView = new TextView(this);
 
         textView.setText("Hello Android!");
 
-        textView.setTextSize(22);
+        textView.setTextSize(26);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
 
-        setContentView(textView);
+        textView.setLayoutParams(layoutParams);
+        constraintLayout.addView(textView);
+
+        setContentView(constraintLayout);
 
 
 
